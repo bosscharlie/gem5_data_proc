f34efdb59a0b3a6fd4a6faa3b1472873b61daf74
Ziang Liu <878574650@qq.com>
2025-02-20 18:20:27 +0800

    update shell scripts for local xs

--- example-scripts/xs-score.sh
+++ example-scripts/xs-score.sh
@@ -4,16 +4,30 @@ ulimit -n 4096
 
 export PYTHONPATH=`pwd`
 
-# example_stats_dir=/nfs-nvme/home/share/wkf/SPEC06_EmuTasks_1215_allbump
-# example_stats_dir=/nfs/home/share/wulingyun/fuck_perf/SPEC06_EmuTasks_new_spec_padding66_1.0_jemalloc_peak_bwaves_2
-example_stats_dir=/nfs/home/share/liyanqin/xs-perf/2403-perf-pf/SPEC06_EmuTasks_0313_0139
+prefix=$1
+base_stats_dir=/nfs/home/liuziang/Workspace/XiangShan-base/spec-result-base
+dev_stats_dir=/nfs/home/liuziang/Workspace/XiangShan-l2hint/spec-result-new-hint
 
-mkdir -p results
+PERF_HOME=/nfs/home/liuziang/Workspace/checkpoints
+gcc12O3_2=/nfs/home/liuziang/Workspace/checkpoints/spec06_rv64gcb_O3_20m_gcc12.2.0-intFpcOff-jeMalloc
+cover1_path_2=$gcc12O3_2/checkpoint-0-0-0/cluster-0-0.json
+cover3_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.3.json
+cover8_path_2=$PERF_HOME/json/gcc12o3-incFpcOff-jeMalloc-0.8.json
 
-tag="xs-fp"
+if [ $prefix = "dev" ]; then
+    stats_dir=$dev_stats_dir
+else
+    stats_dir=$base_stats_dir
+fi
 
-python3 batch.py -s $example_stats_dir -o results/$tag.csv -X
-python3 simpoint_cpt/compute_weighted.py --fp-only \
-    -r results/$tag.csv \
-    -j /nfs/home/share/jiaxiaoyu/simpoint_checkpoint_archive/spec06_rv64gcb_O3_20m_gcc12.2.0-intFpcOff-jeMalloc/checkpoint-0-0-0/cluster-0-0.json \
-    --score results/$tag-score.csv
+rm -r $prefix-results
+mkdir -p $prefix-results
+
+tag="xs-$prefix"
+
+python3 batch.py -s $stats_dir -o $prefix-results/$tag.csv -X
+
+python3 simpoint_cpt/compute_weighted.py \
+    -r $prefix-results/$tag.csv \
+    -j $cover3_path_2 \
+    --score $prefix-results/$tag-score.csv
